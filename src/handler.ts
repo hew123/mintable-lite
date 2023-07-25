2f47ed25c65afeb0a2cce9eda086b89b90d84953
Kieth Hew <keithhew@MacBook-Pro.local>
2023-07-25 23:32:57 +0800

    add 3 endpoibts

--- src/handler.ts
+++ src/handler.ts
@@ -1,14 +1,29 @@
+const RESPONSE_200 = {
+    statusCode: 200,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' // Enable CORS for all domains. You can restrict this to specific domains if needed.
+    }
+}
 
-
-export const mint = async() => {
+const wrapper = (body: object) => {
     const response = {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*' // Enable CORS for all domains. You can restrict this to specific domains if needed.
-        },
-        body: JSON.stringify({ message: 'mint success!' }),
+        ...RESPONSE_200,
+        body: JSON.stringify(body),
     };
     console.log(response);
     return response;
 }
+
+export const mint = async() => {
+    return wrapper({ message: 'mint success'})
+}
+
+export const get = async() => {
+    return wrapper({ message: 'get success'})
+}
+
+export const list = async() => {
+    return wrapper({ message: 'list success'})
+}
+
